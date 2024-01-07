e793f060822f3b99b57d27b51033561980e10e62
castrosgabriel <castro.sgabriel@gmail.com>
2024-01-07 20:03:42 -0300

    Font for windows

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -1,9 +1,11 @@
 import MbLogo from './mb-logo.svg';
 import MeliLogo from './mp-logo.svg';
 import NuBankLogo from './nubank.svg';
+import BrazilianFlag from './brazil-flag.svg';
 
 export {
     MbLogo,
     MeliLogo,
-    NuBankLogo
+    NuBankLogo,
+    BrazilianFlag 
 };
